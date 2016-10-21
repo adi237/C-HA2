c620b7008cab3e1cd94e7ecbe536ea661b42781e
adi237 <adi23792@gmail.com>
2016-10-20 23:39:49 -0400

    Changing file extension

---
+++ HA2.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+
+int cs531_system(char *command);
+
+//Signal Handler
+/*
+*
+* @param
+*/
+void sig_handler(int signum)
+{
+  if (signum == SIGINT)
+   printf("\nreceived SIGINT\n");
+}
+
+/*
+*
+* @param
+*/
+int main(int argc,char *argv[])
+{
+	if(argc < 2)
+	{
+		fprintf(stderr,"Please provide a command.\n");
+		exit(EXIT_FAILURE);
+	}
+    int status;
+    status = cs531_system(argv[1]);
+    return status;
+}
+
+/*
+* This function takes a pointer to character as input(which would contain the command to be executed) 
+* and will execute the command.
+* @param command - pointer to character
+* @return - status of the command execution.
+*/
+int cs531_system(char *command)
+{
+    int status=0;
+    pid_t child_id;
+	pid_t c_id;
+	    
+    child_id = fork();
+    
+    //child logic
+    if(child_id == (pid_t)0){
+		execlp("/bin/sh", "sh", "-c", command, (char *) NULL);
+        
+        //If program reaches this statement it means that execlp has failed.
+        printf("Command execution failed\n");
+    }
+    else{
+        if(child_id < (pid_t)0)
+        {
+            fprintf(stderr,"Fork failed.\n");
+			exit(EXIT_FAILURE);
+        }
+		else
+		{
+			//signal function to bind signalhandler.
+			if (signal(SIGINT, sig_handler) == SIG_ERR)
+				printf("\ncan't catch SIGINT\n");
+			
+			c_id = wait(&status); /*Wait for child to complete.*/
+			printf("Parent: Child %ld exited with status = %d\n",(long)c_id,status);
+		}
+        
+        
+    }
+    return status;
+}
